81ac71533fadde201674cdceb655eea5c685c4aa
Deka Ambia <dekadekadeka@gmail.com>
2020-10-13 00:16:40 -0400

    current user yay

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,6 @@
 module Types
   class QueryType < Types::BaseObject
+    field :current_user, Types::UserType, null: true
     field :fabrics, [Types::FabricType], null: false, description: "All Fabrics"
     field :flosses, [Types::FlossType], null: false
     field :my_projects, [Types::MyProjectType], null: false
@@ -8,6 +9,10 @@ module Types
     field :patterns, [Types::PatternType], null: false
     field :yarns, [Types::YarnType], null: false
     field :users, [Types::UserType], null: false, description: "Users"
+
+    def current_user
+      context[:current_user]
+    end
 
     def fabrics
       Fabric.all
