c94c8ccc1f37def1c2ea647fc531c7fecc03bb86
Deka Ambia <dekadekadeka@gmail.com>
2020-11-13 14:42:01 -0500

    generic images

--- app/graphql/mutations/create_notion.rb
+++ app/graphql/mutations/create_notion.rb
@@ -1,17 +1,17 @@
 module Mutations
-  class CreateNotion < BaseMutation  
-      argument :name, String, required: true
-      argument :brand, String, required: false
-      argument :size, String, required: false
-      argument :description, String, required: false
-      argument :color, String, required: false
+  class CreateNotion < BaseMutation
+    argument :name, String, required: true
+    argument :brand, String, required: false
+    argument :size, String, required: false
+    argument :description, String, required: false
+    argument :color, String, required: false
 
     type Types::NotionType
 
     def resolve(name: nil, brand: nil, size: nil, description: nil, color: nil)
       if context[:current_user].nil?
         raise GraphQL::ExecutionError,
-              "You need to sign in to perform this action"
+          "You need to sign in to perform this action"
       end
 
       Notion.create!(
