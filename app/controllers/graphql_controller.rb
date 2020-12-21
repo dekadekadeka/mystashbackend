bf621044fce10d4a86c8917b8b2383f94ed16e03
Deka Ambia <dekadekadeka@gmail.com>
2020-12-20 21:46:56 -0500

    remove byebug

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -16,7 +16,6 @@ class GraphqlController < ApplicationController
     result = MystashbackendSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue StandardError => e
-    byebug
     raise e unless Rails.env.development?
 
     handle_error_in_development(e)
