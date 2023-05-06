106990e5f366fdf789af3b82b7ea3392cab41d5f
Deka <dekadekadeka@gmail.com>
2023-05-05 22:22:58 -0400

    upgrade everything

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_194431) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_12_20_194431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +21,8 @@ ActiveRecord::Schema.define(version: 2020_12_20_194431) do
     t.string "description"
     t.string "name"
     t.string "pic"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
   end
 
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 2020_12_20_194431) do
     t.string "brand"
     t.string "color"
     t.string "amount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "pic"
   end
@@ -47,8 +46,8 @@ ActiveRecord::Schema.define(version: 2020_12_20_194431) do
   create_table "needles", force: :cascade do |t|
     t.string "needle_type"
     t.string "size"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
   end
 
@@ -58,8 +57,8 @@ ActiveRecord::Schema.define(version: 2020_12_20_194431) do
     t.string "size"
     t.string "description"
     t.string "color"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "pic"
   end
@@ -69,15 +68,15 @@ ActiveRecord::Schema.define(version: 2020_12_20_194431) do
     t.string "number"
     t.string "front_pic"
     t.string "back_pic"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
     t.bigint "pattern_id", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["pattern_id"], name: "index_projects_on_pattern_id"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
@@ -87,8 +86,8 @@ ActiveRecord::Schema.define(version: 2020_12_20_194431) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "yarns", force: :cascade do |t|
@@ -98,8 +97,8 @@ ActiveRecord::Schema.define(version: 2020_12_20_194431) do
     t.string "size"
     t.string "lot"
     t.string "pic"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
   end
 
